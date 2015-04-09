8d62949eca2f9f896bd962fc11b6d6a7e45e9382
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-04-09 13:48:21 +0700

    Set num of threads from console, using `node multithread 2` for 2 threads.

--- multithread.js
+++ multithread.js
@@ -1,10 +1,12 @@
 var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
 
-var n = numCPUs * 1;
+var arg = process.argv.slice(2);
+
+var n = arg[0] || numCPUs * 1;
 
 if (cluster.isMaster) {
-	console.log('--> Your system have ' + numCPUs + ' CPU.');
+	console.log('--> Your system have ' + numCPUs + ' CPU, running on ' + n + ' thread(s).');
 
   // Fork workers.
 	for (var i = 0; i < n; i++) {
