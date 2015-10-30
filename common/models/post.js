83cf460d1d982eaa01753e41e147b5eb9dba990d
Van-Duyet Le <me@duyetdev.com>
2015-10-30 17:36:37 +0700

    Update post loopback model

--- common/models/post.js
+++ common/models/post.js
@@ -2,12 +2,47 @@
 
 module.exports = function (Post) {
 
-  Post.createFakeData = function (faker) {
-    return Post.create({
-      title: faker.lorem.sentence(),
-      content: faker.lorem.paragraph(),
-      image: faker.image.imageUrl()
-    });
-  };
+	Post.createFakeData = function (faker) {
+		return Post.create({
+			title: faker.lorem.sentence(),
+			content: faker.lorem.paragraph(),
+			image: faker.image.imageUrl()
+		});
+	};
 
+	Post.on('dataSourceAttached', function(obj) {
+		var find = Post.find;
+		Post.find = function(filter, cb) {
+			return find.apply(this, arguments);
+		};
+	});
+
+	Post.getRaw = function(page, limit, cb) {
+		var page = page || 0;
+		var limit = limit || 50;
+		var skip = page * limit;
+
+		return Post.find({label: "none"}, { skip: skip, limit: limit });
+	}
+
+	Post.getRawPost = function(page, limit) {
+		return Post.getRaw(page, limit);
+	}
+
+	Post.addLabelById = function(id, label, cb) {
+		Post.update({id: id}, {label: label}, cb);
+	}
+
+	Post.remoteMethod(
+		'getRaw', 
+		{
+			accepts: [
+				{ arg: 'page', type: 'number', default: 0 }, 
+				{ arg: 'limit', type: 'number', default: 50 }
+			],
+			http: {
+				verb: 'get'
+			}
+		}
+	);
 };
