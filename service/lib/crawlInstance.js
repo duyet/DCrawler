b6393381bcaccdc11bf59508b0966f0873a1a7e9
Van-Duyet Le <me@duyetdev.com>
2015-11-12 16:41:31 +0700

    Add crawler instance

---
+++ service/lib/crawlInstance.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var _ = require('lodash');
+var path = require('path');
+var Crawler = require('./crawler');
+
+(function() {
+	var Instance = function(options) {
+		var defaults = {
+			skipDuplicates: true,
+		};
+
+		if (path.existsSync(options || '')) {
+			this.options = require(options);
+		} else {
+			this.options = options || {};
+		}
+
+		this.options = _.merge(defaults, this.options);
+	};
+
+	Instance.prototype.setConfigFrom = function(configFile) {
+		try {
+			var optionFile = require(configFile);
+			if (optionFile) this.options = _.merge(this.options, optionFile);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	Instance.prototype.getConfig = function() {
+		return this.options || {};
+	};
+
+	Instance.prototype.start = function() {
+	};
+
+	module.exports = Instance;
+})call(this);
