5985fffcd340f934c59a1c060a7c0ea2863e8e46
Van-Duyet Le <lvduit08@gmail.com>
2015-11-15 13:59:08 +0700

    Crawler some content from tinhte with xiaomi product

--- service/instance.js
+++ service/instance.js
@@ -1,13 +1,15 @@
-'use strict';
+'use strict'
 
-var _ = require('lodash');
-var args = require('optimist').argv;
+var _ = require('lodash')
+var args = require('optimist').argv
 
-var Instance = require('./lib/crawlInstance');
+var Instance = require('./lib/crawlInstance')
 
 if (args._ || args._.length == 1)
-	var i = new Instance(args._[0]);
-else 
-	var i = new Instance(args);
+  var i = new Instance(args._[0])
+else
+  var i = new Instance(args)
 
-console.log("Config: ", i.getConfig());
+console.log('Config: ', i.getConfig())
+console.log('=======================================')
+i.start()
--- service/lib/crawlInstance.js
+++ service/lib/crawlInstance.js
@@ -1,121 +1,177 @@
-'use strict';
+'use strict'
 
+var path = require('path')
+var fs = require('fs')
+var url = require('url')
 
-var path = require('path');
-var fs = require('fs');
+var _ = require('lodash')
+var mongoose = require('mongoose')
 
-var _ = require('lodash');
-var mongoose = require('mongoose');
+var Crawler = require('./crawler')
+var db = require('../config/db')
 
-var Crawler = require('./crawler');
-var db = require('../config/db');
+;(function () {
+  var Rule = function (selector, type) {
+    this.selector = selector || []
+    if (!_.isArray(this.selector)) this.selector = [this.selector]
 
-(function() {
-	var Rule = function(selector, type) {
-		this.selector = selector || [];
-		if (!_.isArray(this.selector)) this.selector = [this.selector];
+    this.type = type || 'text'
+    this.toString = function () {
+      return this.selector.join(',')
+    }
+  }
 
-		this.type = type || 'text';
-	};
+  var Instance = function (options) {
+    var defaults = {
+      skipDuplicates: true,
 
-	var Instance = function(options) {
-		var defaults = {
-			skipDuplicates: true,
+      product: '',
+      model: 'RawData',
+      urls: [],
+      source: '',
+      rules: {
+        navigator: [],
+        container: [],
+        el: {
+          content: [],
+          date: []
+        }
+      }
+    }
 
-			product: '',
-			model: 'RawData',
-			urls: [],
-			source: '',
-			rules: {
-				navigator: [],
-				container: [],
-				el: {
-					content: [],
-				}
-			}
-		};
+    this.rootDir = path.join(__dirname, '../../')
 
-		this.rootDir = path.join(__dirname, '../../');
+    if (_.isString(options) && fs.existsSync(this.rootDir + options || '')) {
+      this.options = require(this.rootDir + options)
+    } else if (_.isString(options)) {
+      throw Error('Config instance is not found: ' + this.rootDir + options)
+    } else {
+      this.options = options || {}
+    }
 
-		if (_.isString(options) && fs.existsSync(this.rootDir + options || '')) {
-			this.options = require(this.rootDir + options);
-		} else if (_.isString(options)) {
-			throw Error("Config instance is not found: " + this.rootDir + options);
-		}
-		else {
-			this.options = options || {};
-		}
+    this.options = _.merge(defaults, this.options)
 
-		this.options = _.merge(defaults, this.options);
+    // Make rules instance of Rule
+    var that = this
+    if (this.options.rules) {
+      if (this.options.rules.el) {
+        _.each(this.options.rules.el, function (value, key) {
+          console.log(key, value)
+          if (value instanceof Rule === false) that.options.rules.el[key] = new Rule(value)
+        })
+      }
+    }
 
-		// Make rules instance of Rule
-		var that = this;
-		if (this.options.rules) {
-			if (this.options.rules.el) {
-				_.each(this.options.rules.el, function(value, key) {
-					console.log(key, value);
-					if (value instanceof Rule === false) that.options.rules.el[key] = new Rule(value);
-				});
-			}
-		}
+    // Model
+    this.models = {}
+    this.models.queue = require('../models/queue')
+    this.models.contents = require('../models/contents')
+  }
 
-		// Model
-		this.models = {};
-		this.models.queue = require('../models/queue');
-		this.models.contents = require('../models/contents');
-	};
+  Instance.prototype.setConfigFrom = function (configFile) {
+    try {
+      var optionFile = require(configFile)
+      if (optionFile) this.options = _.merge(this.options, optionFile)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
-	Instance.prototype.setConfigFrom = function(configFile) {
-		try {
-			var optionFile = require(configFile);
-			if (optionFile) this.options = _.merge(this.options, optionFile);
-		} catch (e) {
-			console.log(e);
-		}
-	};
+  Instance.prototype.getConfig = function () {
+    return this.options || {}
+  }
 
-	Instance.prototype.getConfig = function() {
-		return this.options || {};
-	};
+  Instance.prototype.updateNavigatorRules = function (rule) {
+    if (!_.isArray(rule)) rule = [rule]
 
-	Instance.prototype.updateNavigatorRules = function(rule) {
-		if (!_.isArray(rule)) rule = [rule];
+    this.options.rules.navigator = rule
+  }
 
-		this.options.rules.navigator = rule;
-	};
+  Instance.prototype.updateContentRules = function (content) {
+    if (!_.isArray(content)) content = [content]
 
-	Instance.prototype.updateContentRules = function(content) {
-		if (!_.isArray(content)) content = [content];
+    this.options.rules.content = content
+  }
 
-		this.options.rules.content = content;
-	};
+  Instance.prototype.getContainerRules = function () {
+    if (!this.options.rules) return false
+    if (!this.options.rules.container) return false
 
-	Instance.prototype.getContentXPath = function(rules) {
-		if (!_.isArray(rules)) rules = [rules];
+    return this.options.rules.container.join(',')
+  }
 
-		return rules.join(',');
-	};
+  Instance.prototype.getElRules = function (el) {
+    if (!this.options.rules) return false
+    if (!this.options.rules.el) return false
+    if (!this.options.rules.el[el]) return false
 
-	Instance.prototype.start = function() {
-		this.instanceConfig = this.options;
+    return this.options.rules.el[el].toString()
+  }
 
-		var that = this;
-		this.instanceConfig.callback = function(error, result, $) {
-			if (!result || !result.request) {
-				return;
-			}
+  Instance.prototype.preprocessTextContent = function (text) {
+    text = text || ''
+    return text.trim()
+  }
 
-			var currentUrl = result.request.href || '';
-			// =====================================================
-			// Parse content
-			var container = $(that.getContentXPath(that.rules.container));
-			if (container.length) {
-				container.each(function(index, block) {
-						
-				});
-			}
-		}
-	};
+  Instance.prototype.removeQuoteFromDom = function (dom) {
+    dom = dom || false
+    if (dom) dom.remove('.bbCodeQuote')
 
-	module.exports = Instance;
-}).call(this);
+    return dom
+  }
+
+  Instance.prototype.getContentBySelector = function (html, selectorRule, defaultReturn) {
+    var defaultReturn = defaultReturn || ''
+    var selector = selectorRule instanceof Rule ? selectorRule.toString() : selectorRule
+
+    console.log('--> ', selector)
+
+    if (!html) return defaultReturn
+    if (!html.find(selector)) return defaultReturn
+
+    // if (this.removeQuote) 
+    html = this.removeQuoteFromDom(html)
+
+    return this.preprocessTextContent(html.find(selector).text() || defaultReturn)
+  }
+
+  Instance.prototype.start = function () {
+    this.instanceConfig = this.options
+
+    console.log('Starting')
+
+    var that = this
+    this.instanceConfig.callback = function (error, result, $) {
+      if (!result || !result.request) {
+        return
+      }
+
+      var currentUrl = result.request.href || ''
+      // =====================================================
+      // Parse content
+      var container = $(that.getContainerRules())
+      if (container.length) {
+        container.each(function (index, block) {
+          var collectData = {
+            parsedUrl: result.request.href,
+            content: that.getContentBySelector($(block), that.getElRules('content'), 'nocontent')
+          }
+
+          console.log(collectData)
+        })
+      }
+    }
+
+    if (this.instanceConfig.urls.length == 0) {
+      console.log('There are no URLs in queue!')
+    }
+
+    this.c = new Crawler(this.instanceConfig)
+    for (var i = 0; i < this.instanceConfig.urls.length; i++) {
+      console.log('Add link to queue: ', this.instanceConfig.urls[i])
+      this.c.queue(this.instanceConfig.urls[i])
+    }
+  }
+
+  module.exports = Instance
+}).call(this)
