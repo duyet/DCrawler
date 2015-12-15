dd0e6dad7e420f580e70a3c8c90e8ec0afc293f3
Van-Duyet Le <me@duyetdev.com>
2015-12-15 18:49:28 +0700

    Add test, modify instance

--- instance.js
+++ instance.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var fs = require('fs');
+
 var _ = require('lodash')
 var args = require('optimist').argv
 var unirest = require('unirest')
@@ -13,7 +15,15 @@ else
   instance = args
 
 instance.resultCallback = function (data) {
+ // Get result object by data.toObject()
  console.log("==>", data.toObject());
+ var dataLine = data.get('url') + "\t"
+    + data.get('title') + "\t"
+    + data.get('price') + "\t"
+    + data.get('datetime') + "\t"
+    + data.get('content') + "\n";
+
+ fs.appendFile('tmp/muaban-net.txt', dataLine);
 }
 
 var i = new Instance(instance)
